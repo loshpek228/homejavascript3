80a361866a8796e5d65f5aa996a983fd9a3892c1
loshpek228 <sasatagaev9@gmail.com>
2024-02-16 15:42:52 +0600

    initial

---
+++ script.js
@@ -0,0 +1,258 @@
+const obj = {
+    name: 'Sasha',
+    last_name: 'Tagaev',
+    age:15,
+    phone: '+996 556 15 07 08',
+    meried: false,
+    gender: 'male',
+    address: {
+        street: 'G.Aitieva',
+        number: 78,
+        country: 'KG',
+        region: 'osh',
+        city: 'osh'
+    },
+    workplace:{
+        name: 'geeks',
+        address: {
+            street: 'abdimatalip amatova',
+            number: 2,
+            country: 'KG',
+            region: 'osh',
+            city: 'osh'
+        },
+     
+    },
+    frends: [
+        {
+            name: 'Arsen',
+            last_name: 'Kalcharoev',
+            age:15,
+            phone: '+996 772 19 95 04',
+            meried: false,
+            gender: 'male',
+            address: {
+                street: 'Aravanski',
+                number: 311,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+            workplace: {
+                name: 'geeks',
+                address: {
+                    street: 'abdimatalip amatova',
+                    number: 2,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            },
+        },
+        {
+            name: 'Eliza',
+            last_name: 'Abdilabekova',
+            age:16,
+            phone: '+996 706 63 49 66',
+            meried: false,
+            gender: 'female',
+            address: {
+                street: 'wwqqw',
+                number: 23,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+            workplace: {
+                name: 'geeks',
+                address: {
+                    street: 'abdimatalip amatova',
+                    number: 2,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            },
+        },
+        {
+            name: 'Chyngyz',
+            last_name: 'Zhumanazarov',
+            age:16,
+            phone: '+996 222 66 77 30',
+            meried: false,
+            gender: 'male',
+            address: {
+                street: 'ibyubbhb',
+                number: 222,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+            workplace: {
+                name: 'geeks',
+                address: {
+                    street: 'abdimatalip amatova',
+                    number: 2,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            },
+        }
+    ],
+    workplace: {
+        name: 'geeks',
+        address: {
+            street: 'abdimatalip amatova',
+            number: 2,
+            country: 'KG',
+            region: 'osh',
+            city: 'osh'
+        },
+    },
+    famaly: [
+        {
+            
+
+            name: 'Raphael',
+            role: 'father',
+            last_name: 'Tagaev',
+            age:55,
+            phone: '+996 555 81 90 02',
+            meried: true,
+            gender: 'male',
+            address: {
+                street: 'i dont now',
+                number: 'i dont now',
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        },
+        {
+            name: 'Elena',
+            role: 'mother',
+            last_name: 'Anatolevna',
+            age:48,
+            phone: '+996 556 20 07 02',
+            meried: false,
+            gender: 'female',
+            address: {
+                street: 'G.Aitieva',
+                number: 78,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        },
+        {
+            name: 'Vladlaena',
+            last_name: 'Tagaeva',
+            role: 'sister 1',
+            age:23,
+            phone: '+996 552 81 90 02',
+            meried: false,
+            gender: 'female',
+            address: {
+                street: 'G.Aitieva',
+                number: 78,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        },
+        {
+            name: 'Karina',
+            last_name: 'Tagaeva',
+            role: 'sister 2',
+            age:18,
+            phone: '+996 555 02 43 60',
+            meried: false,
+            gender: 'female',
+            address: {
+                street: 'G.Aitieva',
+                number: 78,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        },
+        {
+            name: 'Daniel',
+            last_name: 'Tagaev',
+            role: 'brother',
+            age:23,
+            phone: '+996 755 81 90 02 ',
+            meried: false,
+            gender: 'male',
+            address: {
+                street: 'G.Aitieva',
+                number: 78,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        },
+        {
+            name: 'Kirill',
+            last_name: 'Tagaev',
+            role: 'brother 2',
+            age:13,
+            phone: '+996 990 79 11 17 ',
+            meried: false,
+            gender: 'male',
+            address: {
+                street: 'G.Aitieva',
+                number: 78,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+            workplace: {
+                name: 'kirov school',
+                address: {
+                    street: 'Lenina',
+                    number: 312,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            },
+        },
+        {
+            name: 'Maks',
+            last_name: 'Tagaev',
+            role: 'brother 3',
+            age:12,
+            phone: '+996 553 14 14 35',
+            meried: false,
+            gender: 'male',
+            address: {
+                street: 'G.Aitieva',
+                number: 78,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+                
+            },
+            workplace: {
+                name: 'kirov school',
+                address: {
+                    street: 'Lenina',
+                    number: 312,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            },
+        },
+        
+        
+        
+    ]
+    
+
+}
+
+
+console.log(obj.frends[1].phone);
